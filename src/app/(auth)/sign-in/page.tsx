37a7e13297f6a773c57b1e512bf9aaf06d135d56
Sami <samikhan10902@gmail.com>
2025-08-03 15:19:04 +0530

    authentication  done

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from '@/components/ui/card'
+import SignInView from '@/modules/auth/ui/views/sign-in-view'
+import React from 'react'
+
+const page = () => {
+  return (
+   <SignInView/>
+  )
+}
+
+export default page
+
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from '@/components/ui/card'
+import SignUpView from '@/modules/auth/ui/views/sign-up-view'
+import React from 'react'
+
+const page = () => {
+  return (
+ <SignUpView/>
+  )
+}
+
+export default page
