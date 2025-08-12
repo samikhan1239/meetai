2ec5145a0cdb954d170eb2e154006d04daa0831d
Sami <samikhan10902@gmail.com>
2025-08-12 15:20:19 +0530

    agent filter

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -8,8 +8,15 @@ import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-hea
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs";
+import { loadSearchParams } from "@/modules/agents/params";
 
-const Page = async() =>{
+interface Props {
+    searchParams: Promise<SearchParams>;
+};
+
+const Page = async({searchParams}: Props) =>{
+    const filters = await loadSearchParams(searchParams)
 
     const session = await auth.api.getSession({
         headers: await headers(),
@@ -20,7 +27,10 @@ const Page = async() =>{
     }
   const queryClient = getQueryClient
   ();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+    ...filters,
+ 
+  }));
    
 
     return (
--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,13 +1,28 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { PlusIcon } from "lucide-react"
+import { PlusIcon, XCircleIcon } from "lucide-react"
 import { useState } from "react";
 import { NewAgentDialog } from "./new-agent-dialog";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { AgentSearchFilter } from "./agents-search-filter";
+import { DEFAULT_PAGE } from "@/constants";
+
 
 
 export const  AgentsListHeader =() => {
+  const[filters, setFilters] = useAgentsFilters();
     const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const isAnyFilterModified =!!filters.search;
+
+    const onClearFilters = () => {
+      setFilters({
+        search: "",
+        page: DEFAULT_PAGE,
+      })
+
+    }
       return (
         <>
         <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}/>
@@ -19,6 +34,17 @@ export const  AgentsListHeader =() => {
         New Agent
     </Button>
 
+</div>
+<div className ="flex items-center gap-xx-2 p-1">
+<AgentSearchFilter/>
+{isAnyFilterModified && (
+  <Button  variant ="outline" size="sm" onClick={onClearFilters} >
+    <XCircleIcon />
+    Clear
+  </Button>
+)
+
+}
 </div>
         </div>
         </>
