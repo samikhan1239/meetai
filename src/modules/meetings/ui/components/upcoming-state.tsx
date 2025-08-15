f68998a557cac090900e2cb77f0fb106c3186a95
Sami <samikhan10902@gmail.com>
2025-08-15 20:30:15 +0530

    call

--- src/modules/meetings/ui/components/upcoming-state.tsx
+++ src/modules/meetings/ui/components/upcoming-state.tsx
@@ -26,7 +26,7 @@ export const UpcomingState =({
             description ="Once you start this meeting, a summary will appear here"
             />
 
-            <div className ="flex flex-col-reverse lg:flex-row lg:justify-ccenter items-center gap-2 w-full">
+            <div className ="flex flex-col-reverse lg:flex-row lg:justify-center items-center gap-2 w-full">
                 <Button
                 variant = "secondary"
                 className="w-full lg:w-auto"
