c0faf48a5190de276332a93bd61be58d3a12f22c
Sami <samikhan10902@gmail.com>
2025-08-18 19:52:20 +0530

    completed state

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -7,19 +7,12 @@ import { GeneratedAvatar } from "@/components/generated-avatar"
 import { CircleCheckIcon, CircleXIcon, ClockArrowUpIcon, ClockFadingIcon, CornerDownRightIcon, CornerRightDownIcon, LoaderIcon, VideoIcon } from "lucide-react"
 import { Badge } from "@/components/ui/badge"
 import { MeetingGetMany } from "../../type"
-import { cn } from "@/lib/utils"
+import { cn, formatDuration } from "@/lib/utils"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-function formatDuration(seconds: number){
-    return humanizeDuration(seconds * 1000, {
-        largest:1,
-        round: true,
-        units:["h","m","s"],
 
-    });
-};
 const statusIconMap ={
     upcoming:ClockArrowUpIcon,
     active:LoaderIcon,
