eb02240d9bc351ff170997cb17de304492aff77e
Sami <samikhan10902@gmail.com>
2025-09-16 11:32:20 +0530

    all done

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async redirects () {
+    return [
+
+      {
+source: "/",
+destination: "/meetings",
+permanent: false,
+
+      }
+    ]
+  }
 };
 
 export default nextConfig;
